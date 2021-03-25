0afff8e9d3ee58509f3e4f07cd2afd0441bd5e92
Jone <ti@brasiltec.ind.br>
2021-03-25 09:35:53 -0300

    Init

--- exemplos/strings.php
+++ exemplos/strings.php
@@ -1,2 +1,12 @@
 <?php
 
+include "../vendor/autoload.php";
+
+use Jone22\SuperTypes\NString;
+
+$string = new NString("CPF é 12345678911");
+echo $string->numbersOnly(); //123456789
+
+if ($string->startsWith("C")) {
+    echo "true";
+}
